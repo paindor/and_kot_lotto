08771078710fe8a79beb609c9cba39e8193e444a
paindor <paindorrr@gmail.com>
2020-01-09 18:15:49 +0900

    first commit

---
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.application
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn.setOnClickListener{
+            Toast.makeText(this,"click",Toast.LENGTH_LONG).show()
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result",
+                ArrayList(LottoNumberMaker.getRandomLottoNumbers())
+            )
+            startActivity(intent)
+        }
+    }
+}
